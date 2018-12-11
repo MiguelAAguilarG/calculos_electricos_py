b23036196791a398edc714887acc652c76fc0b9c
Miguel Angel Aguilar Garcia <migue_ag18@hotmail.com>
2018-12-11 15:51:26 -0600

    Se puso impresor de salida

--- impresores.py
+++ impresores.py
@@ -7,3 +7,12 @@ def impresor_entrada(datos_dict):
 			c = c + 1
 		print('')
 
+def impresor_salida(salida_dict):
+	for n,m in salida_dict.items():
+		print(f'-> Nodo: {n}')
+		c = 0 
+		for x,y in m.items():
+			print(f'{c+1}. {x}: {y}')
+			c = c + 1
+		print('')
+
